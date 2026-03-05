90a71f117667fbae0a0f51bb2acdbadfd7e6211e
zerotherm27-create <zerotherm27@gmail.com>
2026-03-05 23:28:33 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,8 +1,7 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
@@ -25,4 +24,4 @@ module.exports = async function handler(req, res) {
   } catch (e) {
     return res.status(500).json({ error: "Server error" });
   }
-};
+}
